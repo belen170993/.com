cfc17f4fe0eebf2f6e7c91557f0e5f0444b78561
belen170993 <2022012@iespvigil.edu.pe>
2024-07-22 19:06:37 -0500

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,9 @@
+
+    document.addEventListener('DOMContentLoaded', function () {
+        var navbarToggler = document.querySelector('.navbar-toggler');
+        var navbarNav = document.querySelector('#navbarNav');
+
+        navbarToggler.addEventListener('click', function () {
+            navbarNav.classList.toggle('show')
+        });
+    });
